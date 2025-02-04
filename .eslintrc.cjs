e80ae64e548f33994947c7738f9c201871bbbe4f
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-02-04 13:50:38 +0000

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,5 +4,16 @@ const config = {
   parserOptions: {
     project: true,
   },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next/core-web-vitals",
+    // "plugin:@typescript-eslint/recommended-type-checked",
+    // "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+  rules: {
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+  },
 };
+
 module.exports = config;

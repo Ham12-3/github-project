a94ce55086f5d8d74dd63a4dc40ea0c5a6ef738b
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-02-04 15:18:37 +0000

    update

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -1,5 +1,49 @@
 "use client";
 
+import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+} from "~/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Q&A",
+    url: "/qa",
+    icon: Bot,
+  },
+  {
+    title: "Meetings",
+    url: "/meetings",
+    icon: Presentation,
+  },
+
+  {
+    title: "Billing",
+    url: "/billing",
+    icon: CreditCard,
+  },
+];
 export function AppSidebar() {
-  return <div>sidebar</div>;
+  return (
+    <Sidebar collapsible="icon" variant="floating">
+      <SidebarHeader>Logo</SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent></SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
 }

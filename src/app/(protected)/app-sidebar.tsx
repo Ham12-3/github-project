67bf4c044ac04d6a709cf27a051585d5cc437b9c
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-02-06 13:30:07 +0000

    update

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -54,19 +54,19 @@ const projects = [
     name: "Project 1",
   },
   {
-    name: "Project 1",
+    name: "Project 2",
   },
   {
-    name: "Project 1",
+    name: "Project 3",
   },
   {
-    name: "Project 1",
+    name: "Project 4",
   },
   {
-    name: "Project 1",
+    name: "Project 5",
   },
   {
-    name: "Project 1",
+    name: "Project 6",
   },
 ];
 

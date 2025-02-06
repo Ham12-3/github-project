89308ca8237fa78a3ea335ac79948f6873c1301a
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-02-06 13:39:27 +0000

    upda

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Input } from "~/components/ui/input";
+import Image from "next/image";
+import { Button } from "~/components/ui/button";
 
 type FormInput = {
   repoUrl: string;
@@ -18,28 +20,29 @@ const CreatePage = () => {
   }
 
   return (
-    <div className="flex h-full items-center justify-center gap-12">
-      <img
+    <div className="flex h-full items-center justify-center gap-8">
+      <Image
         src="/undraw_github.svg"
         alt="programming image"
-        className="h-56 w-auto"
+        width={180}
+        height={180}
+        className=""
       />
-      <div>
-        <h1 className="text-2xl font-semibold">Link your Github Repository</h1>
-        <p className="text-s text-muted-foreground">
-          Enter the URL of your repository to link it to Katara.ai
-        </p>
-      </div>
-      <div className="h-4"></div>
+      <div className="flex flex-col gap-6">
+        <div>
+          <h1 className="text-xl font-semibold">Link your Github Repository</h1>
+          <p className="text-sm text-muted-foreground">
+            Enter the URL of your repository to link it to Katara.ai
+          </p>
+        </div>
 
-      <div>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Input
             {...register("repoUrl", { required: true })}
             placeholder="Repository URL"
             required
           />
-          <button type="submit">Submit</button>
+          <Button type="submit" className="mt-4">Submit</Button>
         </form>
       </div>
     </div>

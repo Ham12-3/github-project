7094048d8b09977c1dba45d2f440672b3be7b249
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-02-20 11:43:49 +0000

    update

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -1,29 +1,56 @@
-import {GithubRepoLoader} from "@langchain/community/document_loaders/web/github"
-import {Document} from "@langchain/core/documents"
+import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import { Document } from "@langchain/core/documents";
+import { generateEmbedding, summariseCode } from "./gemini";
 
-export const  loadGithubRepo = async(githubUrl: string, githubToken?: string) => {
-const loader = new GithubRepoLoader(githubUrl, {
-    accessToken: githubToken || '',
-    branch: 'main',
-    ignoreFiles: ['package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb'],
+export const loadGithubRepo = async (
+  githubUrl: string,
+  githubToken?: string,
+) => {
+  const loader = new GithubRepoLoader(githubUrl, {
+    accessToken: githubToken || "",
+    branch: "main",
+    ignoreFiles: [
+      "package-lock.json",
+      "yarn.lock",
+      "pnpm-lock.yaml",
+      "bun.lockb",
+    ],
     recursive: true,
-    unknown: 'warn',
-    maxConcurrency : 5
-})
+    unknown: "warn",
+    maxConcurrency: 5,
+  });
 
-const docs = await loader.load()
-return docs
-}
+  const docs = await loader.load();
+  return docs;
+};
 
+export const indexGithubRepo = async (
+  projectId: string,
+  githubUrl: string,
+  githubToken?: string,
+) => {
+  const docs = await loadGithubRepo(githubUrl, githubToken);
 
-export const indexGithubRepo = async(projectId: string, githubUrl: string, githubToken?:string) => {
-    const docs = await loadGithubRepo(githubUrl, githubToken)
+  const allEmbeddings = await generateEmbeddings(docs);
+  await Promise.allSettled(
+    allEmbeddings.map((embedding, index) => {
+      console.log(`processing ${index} of ${allEmbeddings.length}`);
+      if (!embedding) return;
+    }),
+  );
+};
 
-    const allEmbeddings = await  generateEmbeddings(docs)
-}
-
-const generateEmbeddings = async(docs: Document[]) => {
-return await Promise.all(docs.map(async doc => {
-    const summary = await summariseCode
-}))
-}
+const generateEmbeddings = async (docs: Document[]) => {
+  return await Promise.all(
+    docs.map(async (doc) => {
+      const summary = await summariseCode(doc);
+      const embedding = await generateEmbedding(summary);
+      return {
+        summary,
+        embedding,
+        sourceCode: JSON.parse(JSON.stringify(doc.pageContent)),
+        fileName: doc.metadata.source,
+      };
+    }),
+  );
+};
